099a068b811c16c5af0842e318c09e96218e75bc
Abraham Chan <abrahamchan2@gmail.com>
2021-11-09 19:27:14 -0800

    Update NN-Ensemble test script

--- testing.py
+++ testing.py
@@ -3,7 +3,6 @@ from util import (
     model_wrapper as mw,
     mnistmodel as mm,
     cifar10model as cm,
-    cifar100model as ch,
     gtsrbmodel as gb,
 )
 
@@ -14,13 +13,11 @@ def test_model(benchmark):
 
 
 mnist = [mm.AlexNet(), mm.CNN(), mm.LeNet(), mm.NN(), mm.ResNet50(), mm.RNN(), mm.VGG16()]
-cifar10 = [cm.AlexNet(), cm.CNN(), cm.LeNet(), cm.NN(), cm.ResNet50(), cm.RNN(), cm.VGG16()]
-cifar100 = [ch.AlexNet(), ch.CNN(), ch.LeNet(), ch.NN(), ch.ResNet50(), ch.RNN(), ch.VGG16()]
+cifar10 = [cm.ConvNet(), cm.DeconvNet(), cm.MobileNet(), cm.ResNet18(), cm.ResNet50(), cm.VGG11(), cm.VGG16()]
 gtsrb = [gb.AlexNet(), gb.CNN(), gb.LeNet(), gb.NN(), gb.ResNet50(), gb.RNN(), gb.VGG16()]
 
 
 test_model(mnist)
 test_model(cifar10)
-test_model(cifar100)
 test_model(gtsrb)
 
